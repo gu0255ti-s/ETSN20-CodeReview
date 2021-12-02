9fd8c2eeb540dc2d648957fe17c6dd8019f3e0fa
Gustav Tindberg <gu0255ti-s@student.lu.se>
2021-12-02 13:50:52 +0100

    added more data and better instructions

--- java/src/FilterData.java
+++ java/src/FilterData.java
@@ -16,16 +16,20 @@ public class FilterData {
 
     private void run() {
 
+        //Input filename
         Scanner sc = new Scanner(System.in); // System.in is a standard input stream.
-        System.out.print("Enter a filename (without .txt): ");
+        System.out.println("Enter a filename (without .txt): ");
+        System.out.println("avicii or catData");
         String name = sc.nextLine(); // reads string.
 
-        System.out.print("Enter a pattern to filter on: ");
+        //Input filter pattern
+        System.out.println("Enter a pattern to filter on: ");
+        System.out.println("for avicii try a year between 2012 and 2019, for catData try cat");
         String pattern = sc.nextLine(); // reads string.
 
         String fileName = getPath(name+".txt");
 
-        // read file into stream, try-with-resources
+        // read file into stream and filter based on filter pattern
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
             stream.filter(l -> l.contains(pattern))
                     .forEach(System.out::println);
